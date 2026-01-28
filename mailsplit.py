f86c298d3a0cf18cd796c8a8f2e452667a919393
faruktus <klem2@gmx.net>
2026-01-28 02:14:39 +0100

    adding first working version of mailsplit

--- mailsplit.py
+++ mailsplit.py
@@ -1,11 +1,10 @@
 from pypdf import PdfReader, PdfWriter
 
-subject = "pdf 2_untiltwo 5_untilfive"
+subject = "pdf 2_untiltwo 5_untilfive 9_untilnine 15_untilfifteen"
 
 pdflist = subject.split()[1:]
-print(pdflist)
 
-pdfdict={}
+pdflist_cum=[]
 for x in pdflist:
     try:
         number = int(x[:x.find("_")])
@@ -15,24 +14,29 @@ for x in pdflist:
         name = x[x.find("_")+1:] + str(".pdf")
     except:
         print("Name not found")
+
     try:
-        pdfdict[name] = number
+        pdflist_cum.append([number, name])
     except:
-        print("Dictionary can not be created!")
+        print("Creation of list not possible")
 
-print(pdfdict)
+temp_number=0
+for x in pdflist_cum:
+    if temp_number == 0:
+        x.insert(0, 1)
+        temp_number = x[1]
+    else:
+        x.insert(0, temp_number)
+        temp_number = x[1]
 
+with open("combinedminutes.pdf",'rb') as pdf_file:
+    reader = PdfReader(pdf_file)
+    writer = PdfWriter()
 
+    for x in pdflist_cum:
+        for page in reader.pages[x[0]:x[1]+1]:
+            writer.add_page(page)
 
-
-reader = PdfReader("combinedminutes.pdf")
-output_pdf = PdfWriter()
-
-"""
-for k, v in page_dict.items():
-    for page in reader.pages[v[0]:v[1]]:
-        output_pdf.add_page(page)
-    output_pdf.write(k + ".pdf")
-    output_pdf = PdfWriter()
-"""
-
+            with open(x[2], 'wb') as output_pdf:
+                writer.write(output_pdf)
+        writer = PdfWriter()
